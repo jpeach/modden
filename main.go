b83820d4447ddad9c67b907792fc02a7c6e42a72
James Peach <jpeach@apache.org>
2020-02-20 12:03:28 +1100

    Initialize logging flgs.

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/jpeach/modden/cmd"
 )
 
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
 	if err := cmd.NewRootCommand().Execute(); err != nil {
 		// TODO(jpeach): fish the exit code out of the error type.
 		os.Exit(cmd.EX_FAIL)
