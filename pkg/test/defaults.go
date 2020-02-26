b97c0d1c133f1ffc5c700c715b84567bed1c2041
James Peach <jpeach@apache.org>
2020-02-27 08:18:16 +1100

    Apply must.Bytes.

--- pkg/test/defaults.go
+++ pkg/test/defaults.go
@@ -3,14 +3,11 @@ package test
 import (
 	"github.com/jpeach/modden/pkg/builtin"
 	"github.com/jpeach/modden/pkg/doc"
+	"github.com/jpeach/modden/pkg/must"
 )
 
 func DefaultObjectUpdateCheck() *doc.Fragment {
-	data, err := builtin.Asset("pkg/builtin/objectCheck.rego")
-	if err != nil {
-		// TODO(jpeach): send to test listener.
-		panic(err)
-	}
+	data := must.Bytes(builtin.Asset("pkg/builtin/objectCheck.rego"))
 
 	frag, err := doc.NewRegoFragment(data)
 	if err != nil {
