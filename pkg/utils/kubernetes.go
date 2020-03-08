be085f7026637f8e374c8ffe51bdf05b2a2433ce
James Peach <jpeach@apache.org>
2020-03-09 09:31:00 +1100

    Add godoc comments.

--- pkg/utils/kubernetes.go
+++ pkg/utils/kubernetes.go
@@ -2,6 +2,8 @@ package utils
 
 import "k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 
+// NamespaceOrDefault returns the namespace from u, or "default" if u
+// has no namespace field.
 func NamespaceOrDefault(u *unstructured.Unstructured) string {
 	if ns := u.GetNamespace(); ns != "" {
 		return ns
