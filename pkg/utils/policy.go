8f35f0a4552ffe508f7b0d60bc3d7f20519d8de1
James Peach <jpeach@apache.org>
2020-03-19 17:21:18 +1100

    Add Rego module parse helper.

--- pkg/utils/policy.go
+++ pkg/utils/policy.go
@@ -2,6 +2,8 @@ package utils
 
 import (
 	"errors"
+	"fmt"
+	"io"
 	"io/ioutil"
 
 	"github.com/open-policy-agent/opa/ast"
@@ -9,7 +11,7 @@ import (
 
 // ParseModuleFile parses the Rego module in the given file path.
 func ParseModuleFile(filePath string) (*ast.Module, error) {
-	fileData, err := ioutil.ReadFile(filePath)
+	fileData, err := ioutil.ReadFile(filePath) // nolint(gosec)
 	if err != nil {
 		return nil, err
 	}
@@ -20,6 +22,32 @@ func ParseModuleFile(filePath string) (*ast.Module, error) {
 	}
 
 	return fileModule, nil
+}
+
+// ParseCheckFragment parses a Rego string into a *ast.Module. The
+// Rego input is assumed to not have a package declaration so a random
+// package name is prepended to make the parsed module globally unique.
+// ParseCheckFragment can return nil with no error if the input is empty.
+func ParseCheckFragment(input string) (*ast.Module, error) {
+	// Rego requires a package name to generate any Rules.  Force
+	// a package name that is unique to the fragment.  Note that
+	// we also use this to generate a unique filename placeholder
+	// since Rego internals will sometime use this as a map key.
+	moduleName := RandomStringN(12)
+
+	m, err := ast.ParseModule(
+		fmt.Sprintf("internal/check/%s", moduleName),
+		fmt.Sprintf("package check.%s\n%s", moduleName, string(input)))
+	if err != nil {
+		return nil, err
+	}
+
+	// ParseModule can return nil with no error (empty module).
+	if m == nil {
+		return nil, io.EOF
+	}
+
+	return m, nil
 }
 
 // AsRegoCompilationErr attempts to convert this error into a Rego
